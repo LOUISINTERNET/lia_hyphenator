db0ca7530cc7f5ebb36f7bd5e1c5b9e27368e721
Ingo Hollmann <hollmann@louis.info>
2024-12-17 17:07:39 +0100

    licence stuff

--- Classes/Services/HyphenatorService.php
+++ Classes/Services/HyphenatorService.php
@@ -1,6 +1,11 @@
-<?php
+<?php 
 
-declare(strict_types=1);
+/*
+* This file is part of the "lia_hyphenator" Extension for TYPO3 CMS.
+*
+* For the full copyright and license information, please read the
+* LICENSE.txt file that was distributed with this source code.
+*/
 
 namespace LIA\LiaHyphenator\Services;
 
