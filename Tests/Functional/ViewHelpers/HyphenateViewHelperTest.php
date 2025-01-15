1e4dd49d4e3dca13fc153588fae393f8fc55efd8
Ingo Hollmann <hollmann@louis.info>
2025-01-15 16:55:17 +0100

    fix tests

--- Tests/Functional/ViewHelpers/HyphenateViewHelperTest.php
+++ Tests/Functional/ViewHelpers/HyphenateViewHelperTest.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the "lia_hyphenator" Extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
 namespace LIA\LiaHyphenator\Tests\Unit\ViewHelpers;
 
 use PHPUnit\Framework\Attributes\DataProvider;
